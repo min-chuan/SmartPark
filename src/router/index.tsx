e2a2491d176f40f55630585a63a7e0a3ed0a9912
minchuan <minchuan2025@163.com>
2026-02-18 11:11:25 +0800

    chore: 添加路由和相关页面

---
+++ src/router/index.tsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import React from 'react';
+
+const Home = React.lazy(() => import('@/page/Home'));
+const Login = React.lazy(() => import('@/page/Login'));
+const NotFound = React.lazy(() => import('@/page/NotFound'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
